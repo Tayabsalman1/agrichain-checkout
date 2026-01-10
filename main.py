0a22e351a5091e9fef3a376498664f0700891b31
Tayaib <mohammed.tayab@toprankers.com>
2026-01-10 17:13:24 +0530

    Added main file to interact with user to get scanned items

---
+++ main.py
@@ -0,0 +1,26 @@
+#Entry point for the scanned items:
+import re
+
+if __name__ =="__main__":
+    #looping for getting scanned items 
+    print(
+        """Only enter the Scanned items SKUs like ABC.
+         A, B and C being different items
+         Do not add any non-alphabets""")
+
+    while True:
+        print("Enter 1 to entered new sequence, anything else to Exit")
+        user_input = input()
+
+        if user_input == "1":
+            print("Enter new sequence: ")
+            scanned_items = input()
+            if re.match("^[a-zA-Z]+$", scanned_items):
+                print(type(scanned_items))
+                print(f"Scanned items are {scanned_items}")
+            else:
+                print("Enter only Alphabetic sequence")
+        else:
+            print("\n Exiting...")
+            break
+    
